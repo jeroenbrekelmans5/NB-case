258cac2e7f29b3ff7df47b0955f9b877b78d8f07
Jeroen Brekelmans <jeroen.brekelmans@iodigital.com>
2026-03-11 16:27:50 +0100

    Added basic prettier config

---
+++ prettier.config.js
@@ -0,0 +1,46 @@
+export default {
+	// Use single quotes instead of double quotes
+	singleQuote: true,
+
+	// Add semicolons at the end of statements
+	semi: false,
+
+	// Use tabs instead of spaces
+	useTabs: true,
+
+	// Set tab width to 4 spaces
+	tabWidth: 4,
+
+	// Add trailing commas where valid in ES5 (objects, arrays, etc.)
+	trailingComma: 'all',
+
+	// Print width - line length that the formatter will try to stay within
+	printWidth: 100,
+
+	// Ensure consistent line endings (LF)
+	endOfLine: 'lf',
+
+	// Add spaces inside object literals
+	bracketSpacing: true,
+
+	// Put the > of a multi-line HTML element at the end of the last line
+	bracketSameLine: false,
+
+	// Use single quotes in JSX
+	jsxSingleQuote: false,
+
+	// Always include parens around arrow function arguments
+	arrowParens: 'always',
+
+	// Format quoted properties in objects only when necessary
+	quoteProps: 'as-needed',
+
+	// Add a space after the <!-- and before the --> in HTML comments
+	htmlWhitespaceSensitivity: 'css',
+
+	// Format Vue SFC files
+	vueIndentScriptAndStyle: false,
+
+	// Prose wrap - don't wrap markdown text
+	proseWrap: 'preserve',
+}
